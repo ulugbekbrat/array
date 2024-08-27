3e7848ccd06f8ae4f10205c040d2ce2b28ae6261
ulugbekbrat <168886327+ulugbekbrat@users.noreply.github.com>
2024-08-27 10:35:54 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+
+
+// //shanba kemimiz
+
+// // mevalar.push('wasd')
+
+// // mevalar.pop()
+
+// // mevalar.shift()
+
+// // mevalar.unshift('tar')
+
+// console.log(mevalar);
+
+
+
+let mevalar = ['olma','nok', 'tarvuz', 'bodring' ]
+console.log(mevalar)
+
+alert(mevalar)
+let conf =  confirm('araay qoshimi brat');
+
+if(conf === true){
+    mevalar.push('olma');
+}else{
+    mevalar.pop();
+}
+console.log(mevalar);
